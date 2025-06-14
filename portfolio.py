16fc3e48147dac263664feba773706a03fc09162
JOHN SHIVOGO <jonesjorney@gmail.com>
2025-06-14 15:04:48 +0300

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -13,8 +13,13 @@ import base64
 import tiktoken
 from langchain_groq import ChatGroq
 
+st.set_page_config(
+    page_title='J.S', 
+    page_icon='static/images/cropped_circle_image.png', 
+    layout='wide'
+)
+
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-st.set_page_config(page_title = 'ML/AI Research', page_icon = '📊', layout = 'wide')
 llm = ChatGroq(groq_api_key=GROQ_API_KEY, model_name="qwen-qwq-32b")
 MAX_TOKENS = 5000
 
